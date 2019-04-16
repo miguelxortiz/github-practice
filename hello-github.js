c64c4be3fe564599496ddd23a9088f295e9a5a8d
learn <learn@learn05.local>
2019-04-16 16:35:55 -0700

    added a file

---
+++ hello-github.js
@@ -0,0 +1,1 @@
+console.log("my head hurts")
--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,12 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
+function oddOrEven(number){
+if (number%2 === 1){
+    console.log("odd")
+}   else {
+    console.log("even")
 }
-
+}
 oddOrEven(4)
 oddOrEven(11)
 oddOrEven(75)
